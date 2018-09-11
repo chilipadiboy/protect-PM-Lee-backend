213754f74170c8c6371b3c89a25fb0902e478f47
Zhi Yuan Yong <yongzhiyuan@u.nus.edu>
2018-09-11 21:31:35 +0800

    initial commit

---
+++ src/main/java/temp/Animal.java
@@ -0,0 +1,8 @@
+package temp;
+
+public enum Animal {
+    DOG,
+    CAT,
+    BADGER,
+    MAMMOTH
+}
---
+++ src/main/java/temp/Login.java
@@ -0,0 +1,8 @@
+package temp;
+
+import lombok.Data;
+
+@Data
+public class Login {
+    private String echo;
+}
